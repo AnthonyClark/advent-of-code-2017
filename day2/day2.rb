77bf665a08c8db8439c336411713d5ff2bf01c75
Anthony Clark <anthony.clark@shopify.com>
2017-12-24 15:54:55 -0500

    Day 2 part 2 done

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,15 +1,32 @@
 def main 
   input = IO.read("day2_input.txt").split("\n").map{ |l| l.split(" ") }
   
-  sum = 0
-  input.each do |line|
-    line = line.map(&:to_i)
-    min = line.min
-    max = line.max
-    sum += max - min
-  end
+  part_one(input)
+  part_two(input)
 
-  puts sum
+end
+
+def part_one(input)
+  sum = input.inject(0) { |sum, line| sum + min_max_diff(line.map(&:to_i)) }
+  puts "Part 1: #{sum}"
+end
+
+def part_two(input)
+  sum = input.inject(0) { |sum, line| sum + divisor_sum(line.map(&:to_i).sort) }
+  puts "Part 2: #{sum}"
+end
+
+def min_max_diff(line)
+  line.max - line.min
+end
+
+def divisor_sum(line)
+  line.each do |x|
+    line.each do |y|
+      break if y >= x
+      return x/y if x % y == 0
+    end
+  end
 end
 
 main
