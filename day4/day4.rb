083bb1880b11def5d17591ab87d7c2c5f0d20250
Anthony Clark <anthony.clark@shopify.com>
2017-12-25 15:42:03 -0500

    Clean up day4 a bit

--- day4/day4.rb
+++ day4/day4.rb
@@ -8,17 +8,12 @@ def main
 end
 
 def count_valid_phrases(phrases, stop_palindromes: false)
-  phrases.inject(0) do |sum, phrase|
-    valid_phrase?(phrase, stop_palindromes) ? sum + 1 : sum
-  end
+  phrases.inject(0) { |sum, phrase| valid_phrase?(phrase, stop_palindromes) ? sum + 1 : sum }
 end
 
 def valid_phrase?(phrase, stop_palindromes)
   s = Set.new
-  phrase.split(" ").each do |word|
-    return false unless s.add?(stop_palindromes ? word.chars.sort.join : word)
-  end
-  true
+  phrase.split(" ").all? { |word| s.add?(stop_palindromes ? word.chars.sort.join : word) }
 end
 
 main
