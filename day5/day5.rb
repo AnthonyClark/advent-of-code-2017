9ef6b738727a3711fe3f17e5f33a5fc3568e704e
Anthony Clark <anthony.clark@shopify.com>
2017-12-26 15:23:47 -0500

    day5 part 2

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,17 +1,21 @@
 def main
   input = IO.read("day5_input.txt").split("\n").map(&:to_i)
 
-  puts "Part 1 count: #{count_jumps(input)}"
+  puts "Part 1 count: #{count_jumps(input.dup)}"
+  puts "Part 2 count: #{count_jumps(input, part2: true)}"
 end
 
-def count_jumps(input)
-  pc = 0
-  jumps = 0
+def count_jumps(input, part2: false)
+  pc, jumps = 0, 0
 
   while(pc < input.size ) do
     next_pc = pc + input[pc]
     jumps += 1
-    input[pc] += 1
+    if part2 && input[pc] >= 3 then
+      input[pc] -= 1
+    else
+      input[pc] += 1
+    end
     pc = next_pc
   end
 
